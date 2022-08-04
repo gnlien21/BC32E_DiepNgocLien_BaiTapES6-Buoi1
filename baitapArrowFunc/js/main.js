2b9d70603b784894bcb96015b15eb7f577fac2e2
gnlien21 <gnlien21@gmail.com>
2022-08-04 10:33:47 +0700

    dang bi bai arrow

---
+++ baitapArrowFunc/js/main.js
@@ -0,0 +1,35 @@
+const colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
+
+
+
+let danhSachMau = (array, selector) => {
+    let html = '';
+    for (let value of array){
+        html += `<button class="color-button ${value}"></button>`;
+    }
+
+    document.querySelector(selector).innerHTML = html;
+    return html;
+}
+
+danhSachMau(colorList, '#colorContainer');
+
+const arrayButton = document.getElementsByTagName('button');
+
+let doiMau = (array, selector)=>{
+    let newClassName = '';
+    for (let index in array){
+        array[index].onclick = function(){
+            console.log(array[index].classList[1]);
+            newClassName = array[index].classList[1];
+        }
+
+    }
+    document.querySelector(selector).classList.add(newClassName);
+    return newClassName;
+    
+}
+
+doiMau(arrayButton, '#house');
+
+
